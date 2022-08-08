0e99b4672254ba649ff609498ced931ea49dcf08
Dileep C Unnikrishnan <39225025+drdileepunni@users.noreply.github.com>
2022-08-09 03:59:02 +0530

    Added subset parameter to traintest.py

--- traintest.py
+++ traintest.py
@@ -1,16 +1,26 @@
 
 import random
+import pandas as pd
+import numpy as np
 
 split_ratio = 0.7
 random_seed = 123
 
-def train_test_split(data_dict, split_ratio=0.7, random_seed=123):
+def train_test_split(data_dict, subset_prop=1, split_ratio=0.7, random_seed=123):
 
     subj_ids = list(data_dict.keys())
     random.Random(random_seed).shuffle(subj_ids)
     print(f"Random seed: {random_seed}")
     print(f"Split ratio: {split_ratio}")
-
+    
+    # Subsetting
+    pre_subset_count = len(subj_ids)
+    subset_index = round(len(subj_ids)*subset_prop)
+    subj_ids = subj_ids[:subset_index]
+    print(f"""Selected subset proportion: {subset_prop}
+               Using {len(subj_ids)} of available {pre_subset_count} data points""")
+    
+    # Splitting
     split_index = round(len(subj_ids)*split_ratio)
 
     train_samples = subj_ids[:split_index]
